5f64b44000206893b5a3e6014348518fb7fdbb1e
Fleishmann <gameshowplay@gmail.com>
2018-10-26 22:03:14 -0300

    alteracao inclusao de projeto

---
+++ notasAlunos/notasAlunos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notasAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i; //contador
+            double nota = 0, somadasnotas = 0, mediadasnotas = 0;
+
+            //laço de repetição para ler as 5 Notas 
+            for (i = 1; i<=5;i++)
+                {
+                Console.Write("Informe a [{0}ª] Nota: ", i);
+                nota = Double.Parse(Console.ReadLine());
+
+                somadasnotas = somadasnotas + nota;
+                }
+            //calcula a media das notas
+            mediadasnotas = somadasnotas / 5;
+            Console.Write("\nA soma das notas é : " + somadasnotas);
+            Console.Write("\nA media das notas é : " + mediadasnotas);
+            Console.ReadKey();  
+        }
+    }
+}
